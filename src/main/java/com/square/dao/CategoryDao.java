d15b4f9d34aa47da6a05857a8ae7614f41c79e9e
Edwin Garcia <edwindev08@gmail.com>
2023-05-29 23:14:34 -0500

    Adding createCategories method

--- src/main/java/com/square/dao/CategoryDao.java
+++ src/main/java/com/square/dao/CategoryDao.java
@@ -1,5 +1,41 @@
 package com.square.dao;
 
+import com.square.model.TreeNode;
+
 public class CategoryDao {
+
+    public static TreeNode root;
+
+    private static void createCategories() {
+        // Create root category
+        root = new TreeNode("Home Appliances");
+        root.addKeyword("HomeAppliance");
+        root.addKeyword("Appliance");
+
+        // Create subcategories
+        TreeNode majorAppliances = new TreeNode("Major Appliances");
+        majorAppliances.addKeyword("MajorAppliance");
+        majorAppliances.addKeyword("Appliance");
+        TreeNode minorAppliances = new TreeNode("Minor Appliances");
+        minorAppliances.addKeyword("MinorAppliance");
+        minorAppliances.addKeyword("Appliance");
+        TreeNode kitchenAppliances = new TreeNode("Kitchen Appliances");
+        kitchenAppliances.addKeyword("KitchenAppliance");
+        kitchenAppliances.addKeyword("Appliance");
+        TreeNode generalAppliances = new TreeNode("General Appliances");
+        generalAppliances.addKeyword("GeneralAppliance");
+        generalAppliances.addKeyword("Appliance");
+        TreeNode lawnAndGarden = new TreeNode("Lawn & Garden");
+        lawnAndGarden.addKeyword("Lawn");
+        lawnAndGarden.addKeyword("Garden");
+        lawnAndGarden.addKeyword("GardeningTools");
+
+        // Build category hierarchy
+        root.addChild(majorAppliances);
+        root.addChild(minorAppliances);
+        majorAppliances.addChild(kitchenAppliances);
+        majorAppliances.addChild(generalAppliances);
+        minorAppliances.addChild(lawnAndGarden);
+    }
 
 }
